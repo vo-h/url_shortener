b997a83eee977e43772eb22cbd9c24bc7419db88
Hien Vo <hvo@uchicago.edu>
2022-07-12 09:26:56 -0700

    setup.sh

--- .platform/hooks/postdeploy/01_setup_django.sh
+++ .platform/hooks/postdeploy/01_setup_django.sh
@@ -2,8 +2,8 @@
 
 if [[ $EB_IS_COMMAND_LEADER == "true" ]];
 then
-  python manage.py migrate --noinput;
-  python manage.py collectstatic --noinput;
+  python3 manage.py migrate --noinput;
+  python3 manage.py collectstatic --noinput;
 else
     echo "this instance is NOT the leader"
 fi
