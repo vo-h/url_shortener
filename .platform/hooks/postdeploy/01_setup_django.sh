f8b6c87700d61460883fca5cad77a2b1475046ef
Hien Vo <hvo@uchicago.edu>
2022-07-12 12:12:33 -0700

    ec2-private-ip

--- .platform/hooks/postdeploy/01_setup_django.sh
+++ .platform/hooks/postdeploy/01_setup_django.sh
@@ -3,11 +3,6 @@
 if [[ $EB_IS_COMMAND_LEADER == "true" ]];
 then
   source /var/app/venv/*/bin/activate
-
-  # if [[ -z "${DEPLOY_ENV}" ]]; then
-  #   export ENV_VAR=$(/opt/elasticbeanstalk/bin/get-config environment)
-  # fi
-
   python3 manage.py migrate --noinput;
   python3 manage.py createsu;
   python3 manage.py migrate --noinput;
--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -137,21 +137,21 @@ if "amzn" in platform():
     DEBUG = False
     ALLOWED_HOSTS = [".elasticbeanstalk.com", ".eensy.cc", ".admin.eensy.cc"]
 
-    # EC2_PRIVATE_IP = None
-    # try:
-    #     security_token = requests.put(
-    #         'http://169.254.169.254/latest/api/token',
-    #         headers={'X-aws-ec2-metadata-token-ttl-seconds': '60'}).text
+    EC2_PRIVATE_IP = None
+    try:
+        security_token = requests.put(
+            'http://169.254.169.254/latest/api/token',
+            headers={'X-aws-ec2-metadata-token-ttl-seconds': '60'}).text
 
-    #     EC2_PRIVATE_IP = requests.get(
-    #         'http://169.254.169.254/latest/meta-data/local-ipv4',
-    #         headers={'X-aws-ec2-metadata-token': security_token},
-    #         timeout=0.01).text
-    # except requests.exceptions.RequestException:
-    #     pass
+        EC2_PRIVATE_IP = requests.get(
+            'http://169.254.169.254/latest/meta-data/local-ipv4',
+            headers={'X-aws-ec2-metadata-token': security_token},
+            timeout=0.01).text
+    except requests.exceptions.RequestException:
+        pass
 
-    # if EC2_PRIVATE_IP:
-    #     ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
+    if EC2_PRIVATE_IP:
+        ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
 
 else:
     STATIC_URL = 'static/'
