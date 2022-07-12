af05ba2199dd895d6e3eed7545e97b4260ea6037
Hien Vo <hvo@uchicago.edu>
2022-07-12 09:51:18 -0700

    fixed setup env-var

--- .platform/hooks/postdeploy/01_setup_django.sh
+++ .platform/hooks/postdeploy/01_setup_django.sh
@@ -3,6 +3,7 @@
 if [[ $EB_IS_COMMAND_LEADER == "true" ]];
 then
   source /var/app/venv/*/bin/activate
+  export ENV_VAR=$(/opt/elasticbeanstalk/bin/get-config environment)
   python3 manage.py migrate --noinput;
   python3 manage.py collectstatic --noinput;
 else
